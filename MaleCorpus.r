924407a346467a6e5811a4414793c0895647baa5
samueluyemura <91498125+samueluyemura@users.noreply.github.com>
2021-10-11 12:38:59 -0600

    Update MaleCorpus.r

--- MaleCorpus.r
+++ MaleCorpus.r
@@ -1,3 +1,13 @@
+library(gutenbergr)
+library(tidyverse)
+library(tidytext)
+library(dplyr)
+library(ggplot2)
+library(stringr)
+library(tidyr)
+library(textdata)
+data("stop_words")
+
 # Easily change mirror if needed
 gutenmirror <- "http://mirrors.xmission.com/gutenberg" 
 # Load Male Corpus
@@ -16,3 +26,106 @@ MaleCorpus_cleaned <- MaleCorpus[-c(1:900, 21183:21344, 37772:37829, 49883:49931
 MCToken <- MaleCorpus_cleaned %>% 
   mutate(line = row_number()) %>% 
   unnest_tokens(word, text)
+
+# Stop words removed
+MC_SRR <- MCToken %>% anti_join(stop_words)
+
+# ASSINGMENT ----------
+
+## A: TOP 20 NON-STOP WORDS
+MaleCorpusCount <- MC_SRR %>%
+  count(word, sort=TRUE)
+MaleCorpusCount[1:20,]%>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot()+
+  aes(word,n)+
+  geom_col()+
+  coord_flip()+
+  xlab(NULL)+
+  ylab("No. of Occurrences")+
+  ggtitle("Most Common Words in 'Love Stories by Men' Corpus")
+
+### BING LEXICON - QUESTIONS B-D
+BING <- get_sentiments("bing")
+MaleCorpusBING <- MC_SRR %>%
+  inner_join(BING)%>%
+  count(title = title, sentiment) %>%
+  spread(sentiment, n) %>%
+  mutate(sentiment = positive - negative)
+
+## B: 10 MOST NEGATIVE NOVELS
+MaleCorpusBING %>% arrange(sentiment) %>%
+  mutate(title = reorder(title, -sentiment)) %>%
+  head(10) %>%
+  ggplot() +
+  aes(title, sentiment) +
+  geom_col() + 
+  xlab("Title of Work") +
+  ylab("Sentiment (BING)") +
+  ggtitle("10 Most Negative Novels in 'Love Stories by Men' Corpus") +
+  coord_flip()
+
+## C: OVERALL SENTIMENT
+sum(MaleCorpusBING$sentiment)
+
+## D: 
+InfluentialWords <- MC_SRR %>%
+  inner_join(BING)%>%
+  count(word = word, sentiment, sort = TRUE)
+PositiveInfluentialWords <- InfluentialWords %>%
+  filter(sentiment == "positive")
+NegativeInfluentialWords <- InfluentialWords %>%
+  filter(sentiment == "negative")
+  
+
+### NRC LEXICON - QUESTIONS E&F
+NRC <- lexicon_nrc()
+
+## E: TOP 15 JOY & ANGER WORDS
+NRCjoy <- NRC %>%
+  filter(sentiment == "joy")
+WCjoy <- MCToken %>%
+  inner_join(NRCjoy)%>%
+  count(word, sort = TRUE)%>%
+  top_n(15)
+WCjoy%>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot()+
+  aes(word,n)+
+  geom_col()+
+  coord_flip()+
+  xlab(NULL)+
+  ylab("No. of Occurrences")+
+  ggtitle("Top 15 Joyful Words, Male Corpus")
+
+NRCanger <- NRC %>%  
+  filter(sentiment == "anger")
+WCanger <- MCToken%>%
+  inner_join(NRCanger)%>%
+  count(word, sort = TRUE)%>%
+  top_n(15)
+WCanger%>%
+  mutate(word = reorder(word, n)) %>%
+  ggplot()+
+  aes(word,n)+
+  geom_col()+
+  coord_flip()+
+  xlab(NULL)+
+  ylab("No. of Occurrences")+
+  ggtitle("Top 15 Angry Words, Male Corpus")
+
+## F: WHICH NOVEL IN CORPUS HAS HIGHEST DISGUST
+NRCdisgust <- NRC %>%
+  filter(sentiment == "disgust")
+WCdisgust <- MCToken %>%
+  inner_join(NRCdisgust) %>%
+  count(title, sort=TRUE) %>%
+  head(1)
+
+### AFINN LEXICON - QUESTIONS G&H
+AFINN <- lexicon_afinn()
+MaleCorpusAFINN <- MCToken %>%
+  inner_join(AFINN)%>%
+  group_by(title) %>%
+  summarise(sentiment = sum(value))
+
